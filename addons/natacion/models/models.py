d4ed6434a392273490e803a42139f8e5319630ee
twelfetitan <modestluis@gmail.com>
2025-11-04 15:38:14 +0100

    Solucion errores

--- addons/natacion/models/models.py
+++ addons/natacion/models/models.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
 from odoo import models, fields, api
 
-
-class club(models.Model):
+class Club(models.Model):
     _name = 'natacion.club'
     _description = 'club'
 
     name = fields.Char()
     town = fields.Char()
-    member_ids = fields.One2many('natacion.swimmer', 'club_id')
+    member_ids = fields.One2many('res.partner', 'club_id')
 
 
-class category(models.Model):
+class Category(models.Model):
     _name = 'natacion.category'
     _description = 'categoria'
 
@@ -21,11 +19,12 @@ class category(models.Model):
     maximum_age = fields.Integer()
 
 
-class swimmer(models.Model):
-    _name = 'natacion.swimmer'
-    _description = 'nadador'
+class Swimmer(models.Model):
+    # _name = 'natacion.swimmer'
+    # _description = 'nadador'
+    _inherit = 'res.partner'
 
-    name = fields.Char()
+    is_swimmer = fields.Boolean()
     year_birth = fields.Integer()
     age = fields.Integer(compute="_get_age")
     category_id = fields.Many2one('natacion.category')
@@ -39,50 +38,51 @@ class swimmer(models.Model):
             s.age = int(fields.Date.to_string(fields.Date.today()).split('-')[0]) - s.year_birth
 
 
-class besttime(models.Model):
+class BestTime(models.Model):
     _name = 'natacion.besttime'
     _description = 'mejor tiempo por estilo'
 
-    swimmer_id = fields.Many2one('natacion.swimmer')
+    swimmer_id = fields.Many2one('res.partner')
     style_id = fields.Many2one('natacion.style')
     time = fields.Integer()
 
 
-class style(models.Model):
+class Style(models.Model):
     _name = 'natacion.style'
     _description = 'estilo'
 
     name = fields.Char()
-    best_swimmer_ids = fields.Many2many(comodel_name='natacion.swimmer', 
-                                    relation='natacion_style_swimmer_rel', 
-                                    column1='style_id', 
-                                    column2='swimmer_id')
-    
+    best_swimmer_ids = fields.Many2many(
+        comodel_name='res.partner',
+        relation='natacion_style_swimmer_rel',
+        column1='style_id',
+        column2='swimmer_id'
+    )
 
 
-class championship(models.Model):
+class Championship(models.Model):
     _name = 'natacion.championship'
     _description = 'campeonato'
 
     name = fields.Char()
     club_ids = fields.Many2many(
-                            comodel_name='natacion.club',
-                            relation='natacion_championship_club_rel',
-                            column1='championship_id',
-                            column2='club_id'
+        comodel_name='natacion.club',
+        relation='natacion_championship_club_rel',
+        column1='championship_id',
+        column2='club_id'
     )
     swimmer_ids = fields.Many2many(
-                            comodel_name='natacion.swimmer',
-                            relation='natacion_championship_swimmer_rel',
-                            column1='championship_id',
-                            column2='swimmer_id'
+        comodel_name='res.partner',
+        relation='natacion_championship_swimmer_rel',
+        column1='championship_id',
+        column2='swimmer_id'
     )
     session_ids = fields.One2many('natacion.session', 'championship_id')
     start_date = fields.Char()
     end_date = fields.Char()
 
 
-class session(models.Model):
+class Session(models.Model):
     _name = 'natacion.session'
     _description = 'sesion'
 
@@ -92,7 +92,7 @@ class session(models.Model):
     event_ids = fields.One2many('natacion.event', 'session_id')
 
 
-class event(models.Model):
+class Event(models.Model):
     _name = 'natacion.event'
     _description = 'prova'
 
@@ -100,11 +100,16 @@ class event(models.Model):
     style_id = fields.Many2one('natacion.style')
     category_id = fields.Many2one('natacion.category')
     session_id = fields.Many2one('natacion.session')
-    swimmer_ids = fields.Many2many('natacion.swimmer', 'natacion_event_swimmer_rel', 'event_id', 'swimmer_id')
+    swimmer_ids = fields.Many2many(
+        'res.partner',
+        'natacion_event_swimmer_rel',
+        'event_id',
+        'swimmer_id'
+    )
     series_ids = fields.One2many('natacion.series', 'event_id')
 
 
-class series(models.Model):
+class Series(models.Model):
     _name = 'natacion.series'
     _description = 'serie'
 
@@ -113,11 +118,11 @@ class series(models.Model):
     result_ids = fields.One2many('natacion.result', 'series_id')
 
 
-class result(models.Model):
+class Result(models.Model):
     _name = 'natacion.result'
     _description = 'resultado'
 
-    swimmer_id = fields.Many2one('natacion.swimmer')
+    swimmer_id = fields.Many2one('res.partner')
     series_id = fields.Many2one('natacion.series')
     time = fields.Integer()
     rank = fields.Integer()
